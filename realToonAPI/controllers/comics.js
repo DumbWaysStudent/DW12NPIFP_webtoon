85fa6dfa10e5b47aa9e45a3a8c46db041e4d70e6
andikas95-dev <andikaperdana95@gmail.com>
2019-10-16 21:29:04 +0700

    add search webtoon implementation

--- realToonAPI/controllers/comics.js
+++ realToonAPI/controllers/comics.js
@@ -2,19 +2,44 @@ const models = require('../../models')
 const Webtoons = models.comics
 const Users = models.users
 // const Genres = models.genres
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op;
+
 
 exports.index = (req, res) => {
-    Webtoons.findAll({
-        include: [{
-            model: Users,
-            as: "created_by",
-        }
-            // {
-            //     model: Genres,
-            //     as: "genre_id"}
-        ]
-    }).then(result => res.send(result))
+    const title = req.query.title
+    if (title) {
+        //Untuk Pencarian title
+        Webtoons.findAll({
+            where: { title: { [Op.like]: `%${title}%` } },
+            include: [{
+                model: Users,
+                as: "created_by",
+            }]
+        })
+            .then(result => res.send(result))
+    } else {
+        //jika tidak ada maka dikeluarkan semua value
+        Webtoons.findAll({
+            include: [{
+                model: Users,
+                as: "created_by",
+            }]
+        }).then(result => res.send(result))
+    }
 }
+
+// Webtoons.findAll({
+//     include: [{
+//         model: Users,
+//         as: "created_by",
+//     }
+//         // {
+//         //     model: Genres,
+//         //     as: "genre_id"}
+//     ]
+// }).then(result => res.send(result))
+
 
 // exports.show = (req, res) => {
 //     Webtoons.findOne({
